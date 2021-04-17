b4d1b5167a6d4d72a061d17a049fddc4ecf025bd
Chris <chrislin1015@gmail.com>
2021-04-17 21:58:04 +0800

    Add HeatDistrotion post processing effect and noise textures

---
+++ Assets/Scripts/HeatDistrotion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace Sigono.Framework
+{
+    [Serializable]
+    [PostProcess(typeof(HeatDistrotionRenderer), PostProcessEvent.AfterStack, "LINK/HeatDistrotion")]
+    public class HeatDistrotion : PostProcessEffectSettings
+    {
+        [Range(0.0f, 1.0f), Tooltip("Split RGB")]
+        public FloatParameter splitRgb = new FloatParameter { value = 0.05f };
+
+        [Range(0.0f, 1.0f), Tooltip("Strength")]
+        public FloatParameter strength = new FloatParameter { value = 0.2f };
+
+        public TextureParameter noiseTex = new TextureParameter { defaultState = TextureParameterDefault.Black };
+    }
+
+    public sealed class HeatDistrotionRenderer : PostProcessEffectRenderer<HeatDistrotion>
+    {
+        Shader _shader = Shader.Find("LINK/Post-Process/HeatDistrotion");
+
+        public override void Render(PostProcessRenderContext context)
+        {
+            if (_shader == null || context == null)
+                return;
+
+            PropertySheet propertySheet = context.propertySheets.Get(_shader);
+            if (propertySheet == null)
+                return;
+
+            propertySheet.properties.SetFloat("_SplitRGB", settings.splitRgb);
+            propertySheet.properties.SetFloat("_Strength", settings.strength);
+
+            Texture texParameter = settings.noiseTex.value;
+            if (texParameter == null)
+            {
+                TextureParameterDefault def = settings.noiseTex.defaultState;
+                switch (def)
+                {
+                    case TextureParameterDefault.Black:
+                        texParameter = RuntimeUtilities.blackTexture;
+                        break;
+                    case TextureParameterDefault.White:
+                        texParameter = RuntimeUtilities.whiteTexture;
+                        break;
+                    case TextureParameterDefault.Transparent:
+                        texParameter = RuntimeUtilities.transparentTexture;
+                        break;
+                }
+            }
+            propertySheet.properties.SetTexture("_NoiseTex", texParameter);
+            context.command.BlitFullscreenTriangle(context.source, context.destination, propertySheet, 0);
+        }
+    }
+}
---
+++ Assets/Shaders/HeatDistrotionPP.shader
@@ -0,0 +1,55 @@
+﻿Shader "LINK/Post-Process/HeatDistrotion"
+{
+    Properties
+    {
+        _SplitRGB("Split RGB", Float) = 0.005
+        _Strength("Strength", Float) = 0.2
+        _NoiseTex("Noise Texture", 2D) = "" {}
+    }
+
+    HLSLINCLUDE
+
+    #include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
+
+    TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
+    TEXTURE2D_SAMPLER2D(_NoiseTex, sampler_NoiseTex);
+    uniform float _SplitRGB;
+    uniform float _Strength;
+
+    float4 Frag(VaryingsDefault i) : SV_Target
+    {
+        float2 noisePan = i.texcoord + _Time.y * float2(0, -0.2);
+        float3 noise = SAMPLE_TEXTURE2D(_NoiseTex, sampler_NoiseTex, noisePan);
+
+        float2 screenUV = float2(((noise.r * 0.2 + -0.1) * _Strength), ((noise.r * 0.2 + -0.1) * _Strength)) + i.texcoord;
+
+        _SplitRGB *= 0.01;
+        float2 splitRUV = screenUV + float2(_SplitRGB, _SplitRGB);
+        float2 splitGUV = screenUV + float2(_SplitRGB, -_SplitRGB);
+        float2 splitBUV = screenUV + float2(-_SplitRGB, -_SplitRGB);
+
+        float r = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, splitRUV).r;
+        float g = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, splitGUV).g;
+        float b = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, splitBUV).b;
+
+        float4 color = float4(r, g, b, 1.0);
+
+        return color;
+    }
+
+    ENDHLSL
+
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            HLSLPROGRAM
+            #pragma vertex VertDefault
+            #pragma fragment Frag
+            ENDHLSL
+        }
+    }
+}
